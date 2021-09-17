53d3bafa716a5b3e04207f4929ad5d7383428367
rushalikale <rusha.gaikwad@outlook.com>
2021-09-17 16:44:25 +0530

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+//Added new component BootForm
+import { BootFormComponent } from './boot-form/boot-form.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+//Added bootstrap component
+import { AlertModule } from 'ngx-bootstrap/alert';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BootFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    AlertModule.forRoot(),
+    TooltipModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/boot-form/boot-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-boot-form',
+  templateUrl: './boot-form.component.html',
+  styleUrls: ['./boot-form.component.css']
+})
+export class BootFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
